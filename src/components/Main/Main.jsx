06c6ade8a060d27c365f21c7af06860614e4d446
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-10-28 17:42:41 +0300

    Добавляет компонент Main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContentView from '../ContentView/ContentView';
+
+function Main() {
+  return (
+    <main className='main'>
+      <ContentView />
+    </main>
+  );
+};
+
+export default Main;
