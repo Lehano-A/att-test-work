55d5ff6bc6b0d9ae1f5dec04e055bf5e12934a8a
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-10-28 17:38:42 +0300

    Добавляет компонент SubNavigation

---
+++ src/components/SubNavigation/SubNavigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SubNavigation({ children }) {
+  return (
+    <nav className='sub-navigation'>
+      {children}
+    </nav>
+  );
+};
+
+export default SubNavigation;
