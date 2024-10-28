b0463b6deb1721ed45c62bb22df391f37fab359a
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2024-10-28 17:40:35 +0300

    Добавляет компонент ContentView

---
+++ src/components/ContentView/ContentView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SettingPage from '../pages/SettingPage';
+
+function ContentView() {
+  return (
+    <div className='content-view'>
+      <SettingPage />
+    </div>
+  );
+};
+
+export default ContentView;
